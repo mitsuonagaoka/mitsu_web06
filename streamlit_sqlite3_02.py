dc28a96a62c205a293ac77504a55d063affbfdd7
mitsonagaoka <mmaro.mitsu825@gmail.com>
2023-08-11 17:06:25 +0900

    15

--- streamlit_sqlite3_02.py
+++ streamlit_sqlite3_02.py
@@ -13,7 +13,7 @@ from reportlab.lib.fonts import addMapping
 # Set character encoding
 import locale
 
-locale.setlocale(locale.LC_ALL, 'ja_JP.UTF-8')
+# locale.setlocale(locale.LC_ALL, 'ja_JP.UTF-8')
 
 # Connect to the SQLite database
 conn = sqlite3.connect("./data/product30.db")
